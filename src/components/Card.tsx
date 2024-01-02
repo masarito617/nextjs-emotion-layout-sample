87c1fc8802bbce3b4e0a0d048a24c64da1218112
plasma <plasma@plasmanoMacBook-Pro.local>
2024-01-02 14:01:09 +0900

    コンポーネントにスタイルを適用

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,21 @@
 import { css } from '@emotion/react'
 
 const styleCard = css`
+  background: black;
+  font-size: 40px;
+  color: white;
+  width: 400px;
+  height: 80px;
+  margin: 80px;
+  border-radius: 20px;
   display: flex;
   justify-content: center;
-  font-size: 40px;
+  align-items: center;
 `
 
-const Card = () => {
-  return <div css={styleCard}>Test</div>
+const Card = (props: {
+  text: string
+}) => {
+  return <div css={styleCard}>{props.text}</div>
 }
 export default Card
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,23 @@
+import { Global, css } from '@emotion/react'
 import type { AppProps } from 'next/app'
 
+// グローバルのスタイル
+const styleGlobal = css`
+  html,
+  body,
+  textarea {
+    margin: 0;
+    padding: 0;
+    font-family: 'Helvetica Neue', Arial, 'Hiragino Kaku Gothic ProN',
+      'Hiragino Sans', 'BIZ UDPGothic', Meiryo, sans-serif;
+  }
+`
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global styles={styleGlobal} />
+      <Component {...pageProps} />
+    </>
+  )
 }
