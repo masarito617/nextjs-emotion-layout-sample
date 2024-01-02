1dc83655eae2e90a7bca8bc997b1867f1b75a158
plasma <plasma@plasmanoMacBook-Pro.local>
2024-01-02 13:47:28 +0900

    Emotion導入

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: (() => {
+    let compilerConfig = {
+      // Emotion有効化
+      emotion: true,
+    };
+    return compilerConfig;
+  })(),
 }
 
 module.exports = nextConfig
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,12 @@
+import { css } from '@emotion/react'
+
+const styleCard = css`
+  display: flex;
+  justify-content: center;
+  font-size: 40px;
+`
+
 const Card = () => {
-  return <div>Test</div>
+  return <div css={styleCard}>Test</div>
 }
 export default Card
